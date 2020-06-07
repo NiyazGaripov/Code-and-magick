c3d40e4994cdec0ce830da906b66b7d72cb1ef34
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-06-07 19:17:00 +0300

    Draws shadow from cloud

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,21 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.beginPath();
+  ctx.moveTo(140, 20);
+  ctx.lineTo(500, 20);
+  ctx.quadraticCurveTo(500, 50, 530, 50);
+  ctx.lineTo(530, 260);
+  ctx.quadraticCurveTo(500, 260, 500, 290);
+  ctx.lineTo(140, 290);
+  ctx.quadraticCurveTo(140, 260, 110, 260);
+  ctx.lineTo(110, 50);
+  ctx.quadraticCurveTo(140, 50, 140, 20);
+  ctx.closePath();
+  ctx.stroke();
+  ctx.fill();
+
   ctx.fillStyle = '#fff';
   ctx.strokeStyle = '#fff';
   ctx.beginPath();
