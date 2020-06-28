44a5e473c1d43d78f94a77ab7c6fdd8df69641b8
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-06-28 18:08:18 +0300

    Adds listener and finds start soordinates

--- js/drag-and-drop.js
+++ js/drag-and-drop.js
@@ -3,4 +3,12 @@
 (function () {
   var uploadElement = document.querySelector('.upload');
 
+  uploadElement.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startСoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+  });
 })();
