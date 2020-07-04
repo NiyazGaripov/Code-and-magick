bcc78dfcf9de8e6f331c7ec4ceb550474034506a
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-04 20:43:08 +0300

    Creates hxr and adds response type

--- js/backend.js
+++ js/backend.js
@@ -3,6 +3,7 @@
   var URL = 'https://js.dump.academy/code-and-magick';
 
   var save = function (data, onLoad, onError) {
-
+    var xhr = new XMLHttpRequest;
+    xhr.responseType = 'json';
   };
 })();
