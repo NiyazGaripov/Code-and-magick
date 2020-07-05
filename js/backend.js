9e3e132e2c6063b7c5e9ae07f444a762d6621181
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-07-05 13:21:45 +0300

    Adds conditions for determine response status

--- js/backend.js
+++ js/backend.js
@@ -1,16 +1,30 @@
 'use strict';
 (function () {
   var URL = 'https://js.dump.academy/code-and-magick';
+  var SUCCESS_CODE = 200;
 
   var save = function (data, onLoad, onError) {
-    var xhr = new XMLHttpRequest;
+    var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
+    xhr.open('POST', URL);
+
     xhr.addEventListener('load', function () {
-      onLoad(xhr.response);
+      if (xhr.status === SUCCESS_CODE) {
+        onLoad(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
     });
 
-    xhr.open('POST', URL);
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения! Проверьте соединение с интернетом');
+    });
+
     xhr.send();
+  };
+
+  window.backend = {
+    save: save,
   };
 })();
