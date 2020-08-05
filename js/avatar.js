91f62aa98a67f74d565e8edb8bca14d53870424c
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-05 09:31:33 +0300

    Adds check

--- js/avatar.js
+++ js/avatar.js
@@ -8,6 +8,10 @@
 
   fileChooser.addEventListener('change', function () {
     var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
 
+    var matches = FILE_TYPES.some(function (it) {
+     return fileName.endsWith(it);
+    });
   });
 })();
