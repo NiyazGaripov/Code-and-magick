653901fd8357b425e23e4fff24cf788d56c769eb
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-05 09:29:31 +0300

    Adds event listener

--- js/avatar.js
+++ js/avatar.js
@@ -3,4 +3,9 @@
 (function () {
   var fileChooser = document.querySelector('.upload input[type=file]');
   var preview = document.querySelector('.setup-user-pic');
+
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+
+  });
 })();
